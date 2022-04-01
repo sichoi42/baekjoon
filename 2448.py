274bb92bf7cc3f321be65205ecfe521e6f405050
sichoi42 <42.4.sichoi@gmail.com>
2022-04-01 13:27:54 +0900

    :white_check_mark: 04/01
    04/01

--- 2448.py
+++ 2448.py
@@ -2,24 +2,17 @@ import sys
 
 input = sys.stdin.readline
 
-small_star = [
-	[' ', ' ', '*', ' ', ' '],
-	[' ', '*', ' ', '*', ' '],
-	['*', '*', '*', '*', '*']
-]
-
-def star(n, r, c):
-	if n == 3:
-		for i in range(n):
-			for j in range(2 * n - 1):
-				map[r + i][c + j] = small_star[i][j]
-		return
-	star(n // 2, r, c + n // 2)
-	star(n // 2, r + n // 2, c)
-	star(n // 2, r + n // 2, c + n)
-
 n = int(input())
-map = [[' ' for _ in range(2 * n - 1)] for _ in range(n)]
-star(n, 0, 0)
-for r in range(n):
-	print(''.join(map[r]))
+def star(lst, n):
+	if n == 1:
+		print('\n'.join(lst))
+		return
+	tmp = lst
+	len_tmp = len(tmp)
+	lst = []
+	for i in range(len_tmp):
+		lst.append(' ' * len_tmp + tmp[i] + ' ' * len_tmp)
+	for i in range(len_tmp):
+		lst.append(tmp[i] + ' ' + tmp[i])
+	star(lst, n // 2)
+star(['  *  ', ' * * ', '*****'], n // 3)
